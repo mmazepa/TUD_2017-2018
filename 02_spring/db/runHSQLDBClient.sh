edc3d93fe96cf59c73bf929240950801f1f3b7ff
Mariusz Mazepa <mmazepa@sigma.inf.ug.edu.pl>
2018-01-22 10:31:40 +0100

    Wyeliminowanie chwilowego kłopotu z bazą.

--- 02_spring/db/runHSQLDBClient.sh
+++ 02_spring/db/runHSQLDBClient.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp ./db/hsqldb-2.2.4.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
+java -cp ./db/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
